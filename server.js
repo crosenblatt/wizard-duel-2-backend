673a8ee492f4789b86ba830edf94263ae1b03ffe
crosenblatt <rosench13@gmail.com>
2019-02-07 19:17:35 -0500

    created server to accept connections

--- server.js
+++ server.js
@@ -10,34 +10,31 @@ res.send('Chat Server is running on port 3000')
 
 io.on('connection', (socket) => {
 
-console.log('user connected')
+    console.log('user connected, cool')
 
-socket.on("join", function() {
+    socket.on('join', function() {
         console.log("user has joined");
         socket.broadcast.emit('userjoinedthechat');
-})
+    })
 
 
-socket.on('messagedetection', (spellName) => {
+    socket.on('messagedetection', (spellName) => {
 
         console.log(spellName)
         let  message = { "spell" : spellName }
         
         io.emit('message', message )
+
         })
 
-        socket.on('disconnect', function() {
-            console.log("goodbye")
-            socket.broadcast.emit( "userdisconnect" ,' user has left')
-        })
-
-
-
-
+    socket.on('disconnect', function() {
+        console.log("goodbye")
+        socket.broadcast.emit( "userdisconnect" ,' user has left')
     })
 
+})
+
 server.listen(3000,()=>{
-    //console.log('hi')
-    //console.log('Node app is running on port 3000')
+    console.log('Node app is running on port 3000, hi')
 })
 
