a922d23b0e1d9d7dee2f7d5065e61e180cdb39fb
crosenblatt <rosench13@gmail.com>
2019-02-09 15:54:58 -0500

    the server now pings the UIs with updated info. users can also join rooms

--- server.js
+++ server.js
@@ -10,31 +10,43 @@ res.send('Chat Server is running on port 3000')
 
 io.on('connection', (socket) => {
 
-    console.log('user connected, cool')
+    console.log('user connected');
 
-    socket.on('join', function() {
-        console.log("user has joined");
-        socket.broadcast.emit('userjoinedthechat');
+    /*
+    Join: Enters a user into a room, which is an instance of a game
+    There should be two users per room at any time
+    */
+
+    socket.on('join', function(room) {
+        socket.join(room);
+        console.log("user has joined room: " + room);
+        //socket.broadcast.emit('userjoinedthechat');
     })
 
+    /*
+    messagedetection: Used to detect a spell has been cast
+    It detects the spell, then sends it back to all the other clients in the room
+    */
+    socket.on('messagedetection', (spellName, room) => {
 
-    socket.on('messagedetection', (spellName) => {
-
-        console.log(spellName)
-        let  message = { "spell" : spellName }
+        console.log(spellName);
+        let  message = { "spell" : spellName };
         
-        io.emit('message', message )
+        console.log(room + ": " + spellName);
 
-        })
+        socket.broadcast.to(room).emit('message', message );
 
-    socket.on('disconnect', function() {
-        console.log("goodbye")
-        socket.broadcast.emit( "userdisconnect" ,' user has left')
+        });
+
+    /*disconnect: Self explanatory, used when a user exits a room */
+    socket.on('disconnect', function(room) {
+        console.log("goodbye");
+        //socket.broadcast.to(room).emit( "userdisconnect" ,' user has left');
     })
 
 })
 
 server.listen(3000,()=>{
-    console.log('Node app is running on port 3000, hi')
+    console.log('Node app is running on port 3000, hi');
 })
 
