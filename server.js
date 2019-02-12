90c45d26a83616dadbd2eb2d5a195cb10ffb7460
crosenblatt <rosench13@gmail.com>
2019-02-11 21:00:14 -0500

    the server does not allow more than two people to join a room

--- server.js
+++ server.js
@@ -8,19 +8,39 @@ app.get('/', (req, res) => {
 res.send('Chat Server is running on port 3000')
 });
 
+var roomNums = {};
+
 io.on('connection', (socket) => {
 
     console.log('user connected');
 
     /*
     Join: Enters a user into a room, which is an instance of a game
-    There should be two users per room at any time
+    There should be at most two users per room at any time
     */
 
     socket.on('join', function(room) {
+
+        /*
+        If there are already two people in the room, the server rejects the request
+        to join
+        */
+        var numInRoom = 0;
+        if (roomNums[room] == null) {
+            roomNums[room] = 1;
+        } else {
+            numInRoom = roomNums[room];
+            if(numInRoom == 2) {
+                console.log("rejected");
+                return;
+            }
+            roomNums[room] ++;
+        }
+
+        console.log("room " + room + " contains " + roomNums[room] + " people")
+        console.log("accepted");
         socket.join(room);
         console.log("user has joined room: " + room);
-        //socket.broadcast.emit('userjoinedthechat');
     })
 
     /*
@@ -41,7 +61,7 @@ io.on('connection', (socket) => {
     /*disconnect: Self explanatory, used when a user exits a room */
     socket.on('disconnect', function(room) {
         console.log("goodbye");
-        //socket.broadcast.to(room).emit( "userdisconnect" ,' user has left');
+        roomNums[room] --;
     })
 
 })
@@ -49,4 +69,3 @@ io.on('connection', (socket) => {
 server.listen(3000,()=>{
     console.log('Node app is running on port 3000, hi');
 })
-
