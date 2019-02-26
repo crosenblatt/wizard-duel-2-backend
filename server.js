53b120a589a71f9af5587d3f8c44108c9f0afcb0
Vikas Tatineni <vtatinen@purdue.edu>
2019-02-26 12:57:26 -0500

    Fixed Server.js

--- server.js
+++ server.js
@@ -210,28 +210,27 @@ io.on('connection', (socket) => {
 	socket.emit('passwordReset', result);
   });
 
+
+  /*
+  remove a user from a room
+  called when a game ends
+  */
+  socket.on('leave', function(room) {
+    console.log("game over, leaving room");
+    socket.leave(room);
+  });
+
+  /*disconnect: Self explanatory, used when a user exits a room */
+  socket.on('disconnect', function() {
+    console.log("disconnecting from server"); 
+  });
+
 });
 
 process.on('SIGINT', async function() {
 	await account_management.closeDatabaseConnection();
 	process.exit(0);
 });
-
-    /*
-    remove a user from a room
-    called when a game ends
-    */
-    socket.on('leave', function(room) {
-        console.log("game over, leaving room");
-        socket.leave(room);
-    })
-
-    /*disconnect: Self explanatory, used when a user exits a room */
-    socket.on('disconnect', function() {
-        console.log("disconnecting from server"); 
-    })
-
-})
 
 server.listen(3000,()=>{
     console.log('Node app is running on port 3000, hi' );
